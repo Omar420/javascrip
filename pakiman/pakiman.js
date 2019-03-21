d153644d4bdbbbbb5b0b861e8187a58aa865e2df
Omar420 <ogalindez420@gmail.com>
2019-03-21 09:42:06 -0400

    sustitucion de numeros y asignacion de atributos a una clase

---
+++ pakiman/pakiman.js
@@ -0,0 +1,30 @@
+class pakiman
+{
+	constructor(n, v, a)
+	{
+		this.imagen = new Image();
+		this.nombre = n;
+		this.vida = v;
+		this.ataque = a;
+		this.tipo = "tierra";
+
+		this.imagen.src = imagenes[this.nombre];
+	}
+	hablar()
+	{
+		alert(this.nombre);
+	}
+	mostrar()
+	{
+		document.body.appendChild(this.imagen);
+		document.write("<p>");
+		document.write("<strong>" + this.nombre + "</strong><br />");
+		document.write("Vida" + this.vida + "<br />");
+		document.write("Ataque" + this.ataque + "<hr />");
+		document.write("<p>");
+	}
+	interator()
+	{
+		console.log("jajajaja");
+	}
+}
